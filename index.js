14ca75aed4180d4557d652d73988f00c90d8a6f3
toba96 <tohbyy@gmail.com>
2021-02-20 10:12:27 +0100

    guagebar

--- index.js
+++ index.js
@@ -24,18 +24,29 @@ function loadhtml() {
 }
 
 function onNext() {
-	if (!freshApplication) page = 10;
-	else {
+	if (!freshApplication) {
+		page = 9;
+		document.getElementById('guage').style.width = '12rem';
+	} else {
 		if (page < totalPages) page++;
+		document.getElementById('guage').style.width = `${(page + 1) * 1.2}rem`;
 	}
+	document.getElementById('guage-text').textContent = `${page + 1} of 10`;
 
 	getPage();
 }
 
 function onPrevious() {
-	if (!freshApplication) page = 1;
-	else {
+	if (!freshApplication) {
+		page = 1;
+		document.getElementById('guage').style.width = '1.2rem';
+		document.getElementById('guage-text').textContent = `${page} of 10`;
+	} else {
 		if (page > 1) page--;
+		document.getElementById('guage').style.width = `${(page + 1) * 1.2}rem`;
+		document.getElementById('guage-text').textContent =
+			page === 1 ? '1 of 10' : `${page + 1} of 10`;
 	}
+
 	getPage();
 }
